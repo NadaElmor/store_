209f771b7e66a23475b5afa5894abd5726972f23
nada <nada1elmor@gmail.com>
2025-04-04 04:45:49 +0200

    fix conflict and add nav

--- src/Components/home/home.component.ts
+++ src/Components/home/home.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from '../../Models/iproduct';
+
 import { ProductService } from '../../Services/product.service';
 import { CartService } from '../../Services/cart.service';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FormsModule } from '@angular/forms';
-
+import { IProduct } from './../../Models/iproduct';
 
 
 @Component({
   selector: 'app-home',
   imports: [CommonModule, NgxPaginationModule, FormsModule],
-  templateUrl: './home.component.html',
+ templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
@@ -22,9 +22,6 @@ export class HomeComponent implements OnInit {
   totalProduct: number = 0;
   searchTerm: string = '';
 
-  constructor(private productService: ProductService) {
-
-  }
 
 
   isLoading = true;
@@ -45,21 +42,6 @@ export class HomeComponent implements OnInit {
       this.totalProduct = response.count;
       console.log(this.totalProduct);
     });
-  }
-
-  onPageChange(page: number) {
-    this.currentPage = page;
-    this.loadProducts();
-  }
-
-  onSearch() {
-    this.currentPage = 1;
-    this.loadProducts();
-  }
-
-}
-
-  private loadProducts() {
     this.productService.getAllProducts().subscribe({
       next: (response) => {
         this.products = response.data;
@@ -71,15 +53,25 @@ export class HomeComponent implements OnInit {
       }
     });
   }
+  
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.loadProducts();
+  }
 
+  onSearch() {
+    this.currentPage = 1;
+    this.loadProducts();
+  }
   addToCart(product: IProduct) {
     this.cartService.addToCart(product).subscribe({
       next: () => alert("Add to Cart"),
       error: (err) => {alert('Error: ' + err.message)
         console.log(err);
-        
       }
     });
   }
 }
+
+
 
